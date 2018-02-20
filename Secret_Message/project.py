c1605a98e455a98acb6d0acdb3ad03790b6a9fac
Mohamed Marzouk <alhalis60@gmail.com>
2018-02-21 00:28:30 +0200

    Secret Message Project

---
+++ Secret_Message/project.py
@@ -0,0 +1,7 @@
+import os
+def renameFiles():
+	files_list = os.listdir(r"D:\Programming\My Projects\UdacityFullStakePython\Secret_Message\prank")
+	os.chdir("D:\Programming\My Projects\UdacityFullStakePython\Secret_Message\prank")
+	for file in files_list:
+		os.rename(file, file.translate(None, "0123456789"))
+renameFiles()
